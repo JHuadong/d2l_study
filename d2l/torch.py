ec2049b15490acdb616c455d8777f5dc5fba6e97
SamFisher <547958322@qq.com>
2026-09-21 22:16:54 +0800

    completed the chapter on implementing softmax regression from scratch and change the source code in d2l package for better code reusability.

--- d2l/torch.py
+++ d2l/torch.py
@@ -205,7 +205,9 @@ def get_dataloader_workers():
     Defined in :numref:`sec_fashion_mnist`"""
     return 4
 
-def load_data_fashion_mnist(batch_size, resize=None):
+# In most cases, the project structure of those studying this book will differ from that of the author of this book.
+# Therefore, it is necessary to add a path parameter to make the function more flexible.
+def load_data_fashion_mnist(batch_size, root="../data", resize=None):
     """下载Fashion-MNIST数据集，然后将其加载到内存中
 
     Defined in :numref:`sec_fashion_mnist`"""
@@ -214,9 +216,9 @@ def load_data_fashion_mnist(batch_size, resize=None):
         trans.insert(0, transforms.Resize(resize))
     trans = transforms.Compose(trans)
     mnist_train = torchvision.datasets.FashionMNIST(
-        root="../data", train=True, transform=trans, download=True)
+        root=root, train=True, transform=trans, download=True)
     mnist_test = torchvision.datasets.FashionMNIST(
-        root="../data", train=False, transform=trans, download=True)
+        root=root, train=False, transform=trans, download=True)
     return (data.DataLoader(mnist_train, batch_size, shuffle=True,
                             num_workers=get_dataloader_workers()),
             data.DataLoader(mnist_test, batch_size, shuffle=False,
